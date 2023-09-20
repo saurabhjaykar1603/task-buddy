96a081de8218e340e0278af47c81e9b6d1079db8
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-09-20 13:50:41 +0530

    docs : task structure added

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,17 +1,41 @@
-import React from 'react'
-import "./Home.css"
+import React, { useState } from "react";
+import "./Home.css";
 
 function Home() {
+  const [taskList, setTaskList] = useState([
+    { id: 1, 
+      title: "Task 1",
+      description: "description 1 ",
+      priority: "high",
+     },
+     { id: 1, 
+      title: "Task 1",
+      description: "description 1 ",
+      priority: "high",
+     },
+     { id: 1, 
+      title: "Task 1",
+      description: "description 1 ",
+      priority: "high",
+     }
+    ]
+    );
   return (
     <>
       <div className="container">
-<h1>home page</h1>
+        <h1 className="app-title">Task Buddy</h1>
+      </div>
+      <div className="todo-flex-container">
+        <div>
+          <h1 className="text-center">Show List</h1>
+        </div>
+
+        <div>
+          <h1 className="text-center">Add List</h1>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Home
-
-
-
+export default Home;
